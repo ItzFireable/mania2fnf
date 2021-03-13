990b1907f7557cef0b76d7f9ccf24e74bc1856a1
ItzFireable <fireableplayz@gmail.com>
2021-03-13 23:34:35 +0200

    Add 1-8k support (experimental)

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 var parser = require('osu-parser');
 var fs = require('fs');
+
+// Converter
+
 var args = process.argv.slice(2);
 
 var fnf = {}
 
 parser.parseFile(args[0], function (err, beatmap) {
     if (args[0] != null) {
-        let usedBPM = 0;
+        var usedBPM = 0;
+        var keycount = 0;
         
         fnf.song = {}
         fnf.song.notes = []
+
+        var largestNoteSize = 0;
+        beatmap.hitObjects.forEach(function(item, index) {
+            if (beatmap.hitObjects[index].position[0] > largestNoteSize) {
+                largestNoteSize = beatmap.hitObjects[index].position[0]
+            }
+        })
+
+        for (i = 1; i <= 8; i++) {
+            switch (Math.floor(largestNoteSize * i / 512)) {
+                default:
+                    keycount = 0;
+                    console.log('\x1b[36m%s\x1b[0m',"\n Invalid keycount.")
+                    return false;
+                    break;
+                case 0:
+                    keycount = 1;
+                    break;
+                case 1:
+                    keycount = 2;
+                    break;
+                case 2:
+                    keycount = 3;
+                    break;
+                case 3:
+                    keycount = 4;
+                    break;
+                case 4:
+                    keycount = 5;
+                    break;
+                case 5:
+                    keycount = 6;
+                    break;
+                case 6:
+                    keycount = 7;
+                    break;
+                case 7:
+                    keycount = 8;
+                    break;
+            }
+        }   
+        
         beatmap.timingPoints.forEach(function(item, index) {
             if (Math.round(1 / beatmap.timingPoints[index].beatLength * 1000 * 60) > 0 && usedBPM <= 0) {
                 usedBPM = Math.round(1 / beatmap.timingPoints[index].beatLength * 1000 * 60)
@@ -37,18 +83,22 @@ parser.parseFile(args[0], function (err, beatmap) {
             }
         })
 
+        let prevSide = 0;
         beatmap.hitObjects.forEach(function(item, index) {
             let usedIndex = 0;
+
             fnf.song.notes.forEach(function(item2, index2) {
                 if (fnf.song.notes[index2].timePosition <= beatmap.hitObjects[index].startTime) {
                     usedIndex = index2
                 }
             })
+
             fnf.song.notes[usedIndex].sectionNotes[fnf.song.notes[usedIndex].sectionNotes.length] = [
                 Math.round(beatmap.hitObjects[index].startTime),
-                Math.floor(beatmap.hitObjects[index].position[0] * 4 / 512),
+                Math.floor(beatmap.hitObjects[index].position[0] * keycount / 512),
                 beatmap.hitObjects[index].endTime -  beatmap.hitObjects[index].startTime || 0
             ]
+            
         })
         
         fnf.song.bpm = usedBPM
